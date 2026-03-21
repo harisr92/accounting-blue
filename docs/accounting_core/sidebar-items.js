a1bc1e02fb0eeee7996c057c3c3ff47d8fe42464
harisr92 <harisr92@users.noreply.github.com>
2026-03-21 04:32:16 +0000

    deploy: 6d8ff88c79844324879f575bcd23ef0de877eb99

--- docs/accounting_core/sidebar-items.js
+++ docs/accounting_core/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["ledger","reconciliation","tax","traits","types","utils"]};
+window.SIDEBAR_ITEMS = {"mod":["ledger","reconciliation","tax","traits","types","utils"],"static":["ZERO"]};
